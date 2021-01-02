8d17628e35a393dc305699901439e5f259f1ee8a
seastar105 <seastar105@gmail.com>
2021-01-02 19:27:43 +0900

    Update make_user_list.py
    Add Admin exclusion

--- make_user_list.py
+++ make_user_list.py
@@ -5,11 +5,17 @@ from bs4 import BeautifulSoup
 import requests
 
 def main(argc, argv):
-    if argc != 2:
+    if argc != 2 and argc != 3:
         print("Invalid Arguments")
-        print("Usage : python3 make_user_list.py [BOJ Group Member URL]")
+        print("Usage : python3 make_user_list.py [BOJ Group Member URL] (--no-admin)")
         sys.exit(-1)
-    
+    no_admin_flag = False
+    if argc == 3:
+        if argv[2] != "--no-admin":
+            print("Invalid Arguments")
+            print("Usage : python3 make_user_list.py [BOJ Group Member URL] (--no-admin)")
+            sys.exit(-1)
+        no_admin_flag = True
     url = argv[1]
     response = requests.get(url)
     if response.status_code != 200:
@@ -17,6 +23,8 @@ def main(argc, argv):
         sys.exit(-1)
     html = response.text
     soup = BeautifulSoup(html, 'html.parser')
+    if no_admin_flag:
+        soup = soup.find('div', attrs={'class':'row', 'id':'team_member'})
     script_path = os.path.dirname(os.path.abspath(__file__))
     txt_path = os.path.join(script_path, 'total_user_list.txt')
     user_list = []
